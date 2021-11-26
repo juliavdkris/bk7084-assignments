3a833fbb5c9b6fe68b3fb57dc1737ee19f107d8e
Yang <matthiasychen@gmail.com>
2021-11-26 02:21:33 +0100

    ex03

--- 02_transformation/ex03.py
+++ 02_transformation/ex03.py
@@ -7,8 +7,8 @@ from bk7084.graphics import draw
 from bk7084.scene import Mesh
 
 
-window = Window("BK7084: 02-Transformation")
-window.create_camera(Vec3(-15.0, 8.0, 0.0), Vec3(0, 0, 0), Vec3.unit_y(), 60.0)
+window = Window("BK7084: 02-Transformation [ex03]", width=1024, height=1024)
+window.create_camera(Vec3(-100.0, 50.0, 0.0), Vec3(0, 0, 0), Vec3.unit_y(), 60.0)
 
 """
 Assignment 3: Hierarchical transformation
@@ -17,11 +17,11 @@ Assignment 3: Hierarchical transformation
 Please read the part one of the accompanying PDF file for an explanation of this assignment.
 
 Your tasks:
-1. Build transformation matrices (do NOT use built-in functions):
-  a. translation matrix 
-  b. rotation matrix
-  c. scaling matrix
-2. Move the car to reach the red cube and blue cube inside
+1. Build the simplified solar system.
+    a. Rotation of the earth (rotate around Y axis)
+    b. Revolution of the earth (around the sun)
+    c. Rotation of the moon (rotate around Y axis)
+    d. Revolution of the moon (around the the earth)
 """
 
 
@@ -105,49 +105,40 @@ def scale(x: float, y: float, z: float) -> Mat4:
     return Mat4.identity()
 
 
-cube_x = Mesh('./assets/cube.obj', color=PaletteDefault.RedB.as_color())
-cube_x.initial_transformation = Mat4.from_translation(Vec3(8.0, 0.0, 0.0)) * Mat4.from_scale(Vec3(0.2))
-cube_z = Mesh('./assets/cube.obj', color=PaletteDefault.BlueA.as_color())
-cube_z.initial_transformation = Mat4.from_translation(Vec3(0.0, 0.0, 8.0)) * Mat4.from_scale(Vec3(0.2))
-car = Mesh('./assets/car.obj', color=PaletteDefault.RedA.as_color())
+earth = Mesh('./assets/earth.obj', color=PaletteDefault.GreenA.as_color())
+earth.initial_transformation = Mat4.from_scale(Vec3(0.1))
+
+moon = Mesh('./assets/moon.obj', color=PaletteDefault.WhiteB.as_color())
+moon.initial_transformation = Mat4.from_scale(Vec3(0.2))
+
+sun = Mesh('./assets/sun.obj', color=PaletteDefault.YellowA.as_color())
+sun.initial_transformation = Mat4.from_scale(Vec3(0.15))
+
+earth_to_sun = Mat4.from_translation(Vec3(30, 0, 0))
+
+moon_to_earth = Mat4.from_translation(Vec3(10, 0, 0))
+
+
+time = 0.0
 
 
 @window.event
 def on_draw(dt):
-    # Draw a grid of lines
-    for i in range(21):
-        if i == 10:
-            draw(Line([Vec3(-10, -1.1, -10 + i), Vec3(10, -1.1, -10 + i)], (PaletteDefault.RedA.as_color(),)))
-            draw(Line([Vec3(-10 + i, -1.1, -10), Vec3(-10 + i, -1.1, 10)], (PaletteDefault.BlueA.as_color(),)))
-        else:
-            draw(Line([Vec3(-10, -1.1, -10 + i), Vec3(10, -1.1, -10 + i)]))
-            draw(Line([Vec3(-10 + i, -1.1, -10), Vec3(-10 + i, -1.1, 10)]))
-    draw(car, cube_x, cube_z)
+    draw(sun)
+
+    earth.reset_transformation()
+    earth.transformation = Mat4.from_rotation_y(time * 0.9) * earth_to_sun * Mat4.from_rotation_y(time * 1.2)
+    draw(earth)
+
+    moon.reset_transformation()
+    moon.apply_transformation(Mat4.from_rotation_y(time)).then(moon_to_earth).then(Mat4.from_rotation_y(time)).then(earth.transformation)
+    draw(moon)
 
 
 @window.event
-def on_key_press(key, mods):
-    if key == KeyCode.Up:
-        car.apply_transformation(Mat4.from_translation(Vec3(0.5, 0.0, 0.0)))
-
-    if key == KeyCode.Down:
-        car.apply_transformation(Mat4.from_translation(Vec3(-0.5, 0.0, 0.0)))
-
-    if key == KeyCode.Left:
-        car.apply_transformation(Mat4.from_translation(Vec3(0.0, 0.0, -0.5)))
-
-    if key == KeyCode.Right:
-        car.apply_transformation(Mat4.from_translation(Vec3(0.0, 0.0, 0.5)))
-
-    if key == KeyCode.S:
-        car.apply_transformation(Mat4.from_scale(Vec3(0.9, 0.9, 0.9)))
-
-    if key == KeyCode.R:
-        # Ctrl + R to reset transformation
-        if KeyModifier.Ctrl in mods:
-            car.reset_transformation()
-        else:
-            car.apply_transformation(Mat4.from_axis_angle(Vec3.unit_y(), 45.0, True))
+def on_update(dt):
+    global time
+    time += dt
 
 
 app.init(window)
