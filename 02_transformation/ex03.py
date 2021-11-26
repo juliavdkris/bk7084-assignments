ef879e59e1aeec611844058d6f43bc995d172b81
rubenwiersma <r.t.wiersma@tudelft.nl>
2021-11-26 08:40:16 +0100

    Exercise text

--- 02_transformation/ex03.py
+++ 02_transformation/ex03.py
@@ -12,99 +12,49 @@ window.create_camera(Vec3(-100.0, 50.0, 0.0), Vec3(0, 0, 0), Vec3.unit_y(), 60.0
 
 """
 Assignment 3: Hierarchical transformation
-=======
+-----------------------------------------
 
-Please read the part one of the accompanying PDF file for an explanation of this assignment.
+For your final assignment, you will apply the transformations you created
+and the knowledge about compositions to animate a solar system.
 
-Your tasks:
-1. Build the simplified solar system.
-    a. Rotation of the earth (rotate around Y axis)
-    b. Revolution of the earth (around the sun)
-    c. Rotation of the moon (rotate around Y axis)
-    d. Revolution of the moon (around the the earth)
+If you're done, your solar system will follow these conditions:
+    a. The earth (green planet) rotates around its own axis (y-axis).
+    b. The earth also rotates around the sun (yellow 'planet').
+    c. The moon (grey 'planet') rotates around its own axis (y-axis).
+    d. The moon rotates around the earth.
+
+As you might notice, the order of compositions matters a lot for this exercise.
+Another challenge is to rotate the moon around the earth, while the earth rotates around the sun.
+For this, you must *re-use* the transformation matrix you use for the earth.
+
+Don't start without a plan, you will end up with a tangle of code that is very hard to understand.
+Instead, first design your solar system on paper and write out how to apply the transformations.
+If you do it well, you can finish this exercise by only adding about 5 lines of code!
+
+First, copy your finished transformation functions.
+If you have copied these, you should see the sun and earth.
+You won't see the moon, because it is still at the center of space, covered by the sun.
 """
-
+# TODO copy the completed functions from ex01
 
 def translate(x: float, y: float, z: float) -> Mat4:
-    """
-    Creates a translation matrix.
-
-    Args:
-        x (float): Translation along the x-axis.
-        y (float): Translation along the y-axis
-        z (float): Translation along the z-axis
-
-    Returns:
-        Mat4
-    """
-    # mat = Mat4(
-    #     [
-    #         [0, 0, 0, 0],
-    #         [0, 0, 0, 0],
-    #         [0, 0, 0, 0],
-    #         [0, 0, 0, 0]
-    #     ]
-    # )
     return Mat4.identity()
 
-
-def rotate_x(angle: float, degrees=False) -> Mat4:
-    """
-    Creates a rotation matrix around the x-axis.
-
-    Args:
-        angle (float): Rotation angle, in radians by default.
-        degrees (bool): Specifies how we treat the angle.
-
-    Returns:
-        Mat4
-    """
+def rotate_x(angle: float) -> Mat4:
     return Mat4.identity()
 
-
-def rotate_y(angle: float, degrees=False) -> Mat4:
-    """
-    Creates a rotation matrix around the y-axis.
-
-    Args:
-        angle (float): Rotation angle, in radians by default.
-        degrees (bool): Specifies how we treat the angle.
-
-    Returns:
-        Mat4
-    """
+def rotate_y(angle: float) -> Mat4:
     return Mat4.identity()
 
-
-def rotate_z(angle: float, degrees=False) -> Mat4:
-    """
-    Creates a rotation matrix around the z-axis.
-
-    Args:
-        angle (float): Rotation angle, in radians by default.
-        degrees (bool): Specifies how we treat the angle.
-
-    Returns:
-        Mat4
-    """
+def rotate_z(angle: float) -> Mat4:
     return Mat4.identity()
-
 
 def scale(x: float, y: float, z: float) -> Mat4:
-    """
-    Creates a scaling matrix.
-
-    Args:
-        x (float): Scaling factor along x-axis.
-        y (float): Scaling factor along y-axis.
-        z (float): Scaling factor along z-axis.
-
-    Returns:
-        Mat4
-    """
     return Mat4.identity()
 
-
+"""
+Here, we load the planets, no need to adjust anything here yet.
+"""
 earth = Mesh('./assets/earth.obj', color=PaletteDefault.GreenA.as_color())
 earth.initial_transformation = Mat4.from_scale(Vec3(0.1))
 
@@ -114,11 +64,17 @@ moon.initial_transformation = Mat4.from_scale(Vec3(0.2))
 sun = Mesh('./assets/sun.obj', color=PaletteDefault.YellowA.as_color())
 sun.initial_transformation = Mat4.from_scale(Vec3(0.15))
 
-earth_to_sun = Mat4.from_translation(Vec3(30, 0, 0))
+"""
+This is where you can start building up your transformation matrices.
+For example, create a translation matrix for the earth to be placed away from the sun:
+"""
+earth_translation = translate(30, 0, 0)
+# TODO: other transformations you might need
 
-moon_to_earth = Mat4.from_translation(Vec3(10, 0, 0))
-
-
+"""
+You will use this time variable to animate the solar system.
+It is updated for you in the on_update function.
+"""
 time = 0.0
 
 
@@ -126,14 +82,24 @@ time = 0.0
 def on_draw(dt):
     draw(sun)
 
+    """
+    This is where you should apply your transformations the the earth and moon.
+    Remember that you can apply transformations with apply_transformation() and then().
+    Also, you can get the current transformation for an object with
+    >>> earth_transformation = earth.transformation
+    """
     earth.reset_transformation()
-    earth.transformation = Mat4.from_rotation_y(time * 0.9) * earth_to_sun * Mat4.from_rotation_y(time * 1.2)
+    earth.apply_transformation(earth_translation)
+    # TODO: Compute and apply transformations
     draw(earth)
 
     moon.reset_transformation()
-    moon.apply_transformation(Mat4.from_rotation_y(time)).then(moon_to_earth).then(Mat4.from_rotation_y(time)).then(earth.transformation)
+    # TODO: Compute and apply transformations
     draw(moon)
 
+"""
+You don't need to change the code below to finish the assignment.
+"""
 
 @window.event
 def on_update(dt):
