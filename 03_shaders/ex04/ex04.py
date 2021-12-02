7e56611dba03cebec1d698fd1ae5a7c9110a63be
rubenwiersma <r.t.wiersma@tudelft.nl>
2021-12-02 23:16:48 +0100

    Exercise 4 startup text

--- 03_shaders/ex04/ex04.py
+++ 03_shaders/ex04/ex04.py
@@ -12,6 +12,24 @@ window = Window("BK7084: 03-Shaders [ex04]", width=1024, height=1024)
 window.create_camera(Vec3(2.0, 1.0, -2.0), Vec3(0, 0, 0), Vec3.unit_y(), 60.0)
 assignment_directory = os.path.dirname(os.path.abspath(__file__))
 
+"""
+Exercise 4: Advanced fragment shader
+------------------------------------
+
+In the previous exercises you got to know the vertex and fragment shader
+and found out how to combine the two.
+In this exercise, you'll create more advanced looks with the fragment shader.
+You have two tasks. Only do the second task if you have the time.
+1. Implement a rim light effect (https://www.roxlu.com/2014/037/opengl-rim-shader)
+2. Implement a toon color effect (https://www.lighthouse3d.com/tutorials/glsl-12-tutorial/toon-shader-version-ii/)
+
+When you're done with this exercise and still have time left,
+you can learn more about shaders by going through the Book of Shaders (https://thebookofshaders.com/00/)
+This online book with lots of interactive material can help you grasp shaders even better.
+
+For now, open up `ex04.frag` to continue.
+"""
+
 window.default_shader = ShaderProgram(
     VertexShader.from_file(os.path.join(assignment_directory, 'ex04.vert')),
     PixelShader.from_file(os.path.join(assignment_directory, 'ex04.frag'))
