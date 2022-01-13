b0a502119ae0ef780a8ec7d2ea73220ce1d025ae
matthiascy <matthiasychen@gmail.com>
2022-01-13 18:32:34 +0100

    draw graid lines

--- 05_optimization/main.py
+++ 05_optimization/main.py
@@ -28,8 +28,8 @@ building = 0
 
 @window.event
 def on_draw(dt):
-    scene.draw(auto_shadow=True)
-
+    scene.draw_v2(auto_shadow=True)
+    city.grid.draw_grid_line()
 
 @window.event
 def on_gui():
