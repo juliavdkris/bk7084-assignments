b09f6b11131cdca43ede28cae14ed3ba4af393db
rubenwiersma <r.t.wiersma@tudelft.nl>
2023-11-17 16:05:44 +0100

    Clarity exercise 2

--- 01_transformation/ex02.py
+++ 01_transformation/ex02.py
@@ -19,25 +19,22 @@ Exercise 2: Hierarchical transformation
 For your final assignment, you will apply the transformations you created
 and the knowledge about compositions to animate a solar system.
 
-If you're done, your solar system will follow these conditions:
-    a. The earth (green planet) rotates around its own axis (y-axis).
-    b. The earth also rotates around the sun (yellow 'planet').
-    c. The moon (grey 'planet') rotates around its own axis (y-axis).
-    d. The moon rotates around the earth.
+First start the program and see what happens.
+You're supposed to see one shape that resembles a planet. This is the sun.
+There are also an earth and a moon, but they are currently hidden by the sun.
 
-As you might notice, the order of compositions matters a lot for this exercise.
-Another challenge is to rotate the moon around the earth, while the earth rotates around the sun.
-For this, you must *re-use* the transformation matrix you use for the earth.
-
-Don't start without a plan, you will end up with a tangle of code that is very hard to understand.
-Instead, first design your solar system on paper and write out how to apply the transformations.
-If you do it well, you can finish this exercise by only adding about 5 lines of code!
+The goal of this assignment is to use transformation matrices to
+1. Place the earth and moon in the solar system.
+2. Animate the earth and moon.
 
 First, copy your finished transformation functions.
-If you have copied these, you should see the sun and earth.
-You won't see the moon, because it is still at the center of space, covered by the sun.
+If you have copied these, you should see the sun, earth and moon.
+
+After you've copied the transformation functions, scroll down to Task 1.
 """
 # TODO copy the completed functions from ex01
+# Note that the functions below currently return the identity matrix
+# this matrix does not change the shape at all.
 
 def translate(x: float, y: float, z: float) -> Mat4:
     return Mat4.identity()
@@ -61,51 +58,79 @@ cwd = osp.dirname(osp.abspath(__file__))
 Here, we load the planets, no need to adjust anything here yet.
 """
 earth = app.add_mesh(bk.Mesh.load_from(osp.join(cwd, 'assets/earth.obj')))
-earth.set_transform(Mat4.from_scale(Vec3(0.1)))
 earth.set_visible(True)
 
-# moon = app.add_mesh(bk.Mesh.load_from(osp.join(cwd, 'assets/moon.obj')))
-# moon.set_transform(Mat4.from_scale(Vec3(0.2)))
-# moon.set_visible(True)
+moon = app.add_mesh(bk.Mesh.load_from(osp.join(cwd, 'assets/moon.obj')))
+moon.set_visible(True)
 
-# sun = app.add_mesh(bk.Mesh.load_from(osp.join(cwd, 'assets/sun.obj')))
-# sun.set_transform(Mat4.from_scale(Vec3(0.15)))
-# sun.set_visible(True)
-
-"""
-This is where you can start building up your transformation matrices.
-For example, create a translation matrix for the earth to be placed away from the sun:
-"""
-earth_translation = translate(40, 0, 0)
-moon_to_earth = Mat4.from_translation(Vec3(10, 0, 0))
-# TODO: other transformations you might need
+sun = app.add_mesh(bk.Mesh.load_from(osp.join(cwd, 'assets/sun.obj')))
+sun.set_visible(True)
 
 
 """
-You don't need to change the code below to finish the assignment.
-"""
+The on_update function is called every time the screen is updated.
+This happens every few milliseconds - as fast as your computer can handle.
+That means you can use this function to update the solar system and animate it.
 
-earth_transform = earth_translation
-moon_transform = moon_to_earth
+The on_update function gets three parameters:
+- input: this tells the update event what happend. You can ignore it.
+- dt: the change in time since the last update.
+- t: the current time.
 
-"""
-You will use the time variable `t` to animate the solar system.
-It is updated for you in the on_update function.
+You will use the t parameter to animate your solar system. Scroll down for the task.
 """
 @app.event
 def on_update(input, dt, t):
     """
-    This is where you should apply your transformations the the earth and moon.
-    Remember that you can apply transformations with apply_transformation() and then().
-    Also, you can get the current transformation for an object with
-    >>> earth_transformation = earth.transformation
+    Task 1. Place the planets in the right place.
+    ---------------------------------------------
+    We've set up a few transformations to start with:
+    the planets are scaled to have a realistic size and the earth and moon are translated.
+
+    If this is not happening, you might have forgotten to copy your transformation functions above.
+    """
+    earth_scale = scale(0.1, 0.1, 0.1)
+    moon_scale = scale(0.2, 0.2, 0.2)
+    sun_scale = scale(0.15, 0.15, 0.15)
+    earth_translation = translate(40, 0, 0)
+    moon_from_earth_translation = translate(10, 0, 0)
+
+    """
+    We then combine the transformations by multiplying them together
+    and we set the transformation matrices for the planets.
+    
+    TODO: Complete the transformations, so that:
+    - The earth is distance 40 away from the sun.
+    - The moon is distance 10 away from the earth - hint: you can use the earth's transformation matrix.
+    """
+    sun_transform = Mat4.identity()
+    earth_transform = earth_translation
+    moon_transform = moon_from_earth_translation
+
+    """
+    Task 2: Animate the planets
+    ---------------------------
+    Now, we want to animate the planets.
+    
+    We want the following animation:
+    - The earth rotates around its own axis.
+    - The moon rotates around its own axis.
+    - The moon rotates around the earth.
+    - The earth rotates around the sun.
+
+    TODO: Update your transformations above to animate the planets.
+    HINT: Start with a plan. Draw out the relationships on paper.
+          You'll need to combine the transformations in the right way to get the desired result.
+    HINT: You can use the t parameter to animate the planets. 
     """
 
-    # TODO: Compute and earth transformation
+    """
+    This is where the complete transformations are applied to the planets.
+    """
+    sun.set_transform(sun_transform * sun_scale)
+    earth.set_transform(earth_transform * earth_scale)
+    moon.set_transform(moon_transform * moon_scale)
 
-    # TODO: Compute and moon transformation
-
-    # TODO: Apply transformations to the earth and moon
     pass
 
 
