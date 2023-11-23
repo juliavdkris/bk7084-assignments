18ea83258e9febf8a45add64f70f2f9130ec3db3
rubenwiersma <r.t.wiersma@tudelft.nl>
2023-11-23 17:31:45 +0100

    Added comments and explanations

--- 02_advanced_transformation/ex01.py
+++ 02_advanced_transformation/ex01.py
@@ -4,6 +4,19 @@ import sys
 import bk7084 as bk
 from bk7084.math import *
 
+"""
+Exercise 1: Combining transformations and animation
+---------------------------------------------------
+
+Start by opening the exercise. You'll see a car and a village.
+When this exercise is completed, the car will drive through the village
+following a path that you program.
+
+The learning goal of the exercise is to get more understanding
+of transformations and how you can combine them.
+
+Scroll down to the next comment...
+"""
 
 def load_meshes(app, filepath):
     if not os.path.exists(filepath):
@@ -26,77 +39,158 @@ win.set_size(1024, 1024)
 win.set_resizable(True)
 
 app = bk.App()
-camera = app.create_camera(pos=Vec3(4, 64, 4), look_at=Vec3(0, 0, 0), fov_v=60.0, near=0.1, far=500.0)
+camera = app.create_camera(pos=Vec3(0, 64, -24), look_at=Vec3(0, 0, -25), fov_v=60.0, near=0.1, far=500.0)
 camera.set_as_main_camera()
 
 load_meshes(app, bk.res_path("./assets/village/"))
 car = app.add_mesh(bk.Mesh.load_from(bk.res_path("./assets/car.obj")))
 car.set_visible(True)
 
-car_transform = Mat4.from_rotation_y(180.0, True)
+cwd = os.path.dirname(os.path.abspath(__file__))
+arrow_x_mesh = bk.Mesh.load_from(os.path.join(cwd, '../01_transformation/assets/arrow.obj'))
+mtl_red = bk.Material()
+mtl_red.kd = Vec3(1.0, 0.0, 0.0)
+arrow_x_mesh.apply_material(mtl_red)
 
-car_speed = 20.0
-car_turn_speed = 90.0
+arrow_y_mesh = bk.Mesh.load_from(os.path.join(cwd, '../01_transformation/assets/arrow.obj'))
+mtl_green = bk.Material()
+mtl_green.kd = Vec3(0.0, 1.0, 0.0)
+arrow_y_mesh.apply_material(mtl_green)
 
+arrow_z_mesh = bk.Mesh.load_from(os.path.join(cwd, '../01_transformation/assets/arrow.obj'))
+mtl_blue = bk.Material()
+mtl_blue.kd = Vec3(0.0, 0.0, 1.0)
+arrow_z_mesh.apply_material(mtl_blue)
+
+arrows = [
+    app.add_mesh(arrow_x_mesh),
+    app.add_mesh(arrow_y_mesh),
+    app.add_mesh(arrow_z_mesh)
+]
+
+# x-axis
+arrows[0].set_transform(Mat4.from_translation(Vec3(7.0, 0.0, 0.0)) * Mat4.from_scale(Vec3(0.2)) * Mat4.from_rotation_z(-90.0, degrees=True))
+arrows[0].set_visible(True)
+
+# y-axis
+arrows[1].set_transform(Mat4.from_translation(Vec3(0.0, 7.0, 0.0)) * Mat4.from_scale(Vec3(0.2)))
+arrows[1].set_visible(True)
+
+# z-axis
+arrows[2].set_transform(Mat4.from_translation(Vec3(0.0, 0.0, 7.0)) * Mat4.from_scale(Vec3(0.2)) * Mat4.from_rotation_x(90.0, degrees=True))
+arrows[2].set_visible(True)
+
+"""
+Here, we set the initial transformation of the car.
+The car is rotated by 180 degrees around the y axis.
+In our world, the y axis points upwards, so the car is rotated around the vertical axis.
+
+Notice that we give a parameter that tells the function that the angle is in degrees.
+By default, the function expects the angle to be in radians (from 0 to 2pi).
+"""
+car_transform = Mat4.from_rotation_y(180.0, degrees=True)
+
+"""
+The car gets an initial speed and a turn speed.
+Assume 1 unit in the virtual world is 1 meter in the real world.
+So, if the car is translated by [1, 0, 0], it will move 1 meter in the x direction.
+"""
+car_speed = 20.0 # m/s
+car_turn_speed = 90.0 # degrees/s
+
+
+"""
+The path of the car is split up in segments.
+Each segment is either a straight line or a turn.
+Then we define the segment types and the length of each segment.
+
+Task 1: Familiarize yourself with the path
+------------------------------------------
+1. On a piece of paper, draw the path of the car. Don't worry about getting the distances exactly right.
+   Observe what you do when you draw the path.
+   What are the steps you take to turn the instructions into a drawing?
+   E.g., for each element in the list, I look at the type of segment ...
+2. Start the program and see if you can match the path you drew.
+   We've positioned the camera in a top-view setting, so that positive x-axis points to the right
+   and the negative z-axis points to the top of the screen.
+
+Once you're done, continue reading the comments util you see Task 2.
+"""
+segment_type = ['straight', 'right_turn', 'straight', 'right_turn', 'straight', 'left_turn', 'straight', 'right_turn', 'straight']
+segment_length = [7.0, 90.0, 20.5, 90.0, 8.0, 90.0, 36.0, 90.0, 10.0]
+
+"""
+We start with segment 0, which is the first segment if you count from 0.
+"""
 current_segment = 0
 
-#            straight turn_point0, straight, turn_point1, straight, turn_point2, straight, turn_point3, straight
-road = [7.0, 90.0, 20.5, 90.0, 8.0, 90.0, 36.0, 90.0, 10.0]
-
-marched_distance = 0.0
+"""
+And we keep track of the distance the car has covered in the current segment.
+That way, we know when we should proceed to the next segment.
+"""
+distance_covered_in_segment = 0.0
 turned_angle = 0.0
 
+"""
+The on_update function is called every time the screen is updated (every frame).
+This happens every few milliseconds - as fast as your computer can handle.
 
+The on_update function gets three parameters:
+- input: this tells the update event what happend. You can ignore it.
+- dt: the change in time since the last update.
+- t: the current time.
+
+The dt parameter will tell you how much time passed since the last frame was rendered.
+Using the speed defined above and dt, 
+you can calculate how far the car moved since the last frame.
+
+Scroll down for the task.
+"""
 @app.event
 def on_update(input, dt, t):
     global car_transform
     global current_segment
-    global marched_distance
+    global distance_covered_in_segment
     global turned_angle
 
-    if current_segment < len(road):
-        if current_segment % 2 == 0:
-            marched_distance += car_speed * dt
-            if marched_distance > road[current_segment]:
-                marched_distance = 0.0
+    """
+    Task 2: Animating the car
+    -------------------------
+    Animate the car, by adjusting car_transform.
+    If you scroll down, you'll see that car_transform is applied to the car every frame.
+    
+    What's important to know is that the car is positioned at [0, 0, 0] at the start of each update.
+    Fortunately, you know where the car was in a previous frame,
+    because it is stored in car_transform.
+    
+    It is up to you to multiply car_transform with the correct transformation matrix
+    to get it to the right location for the current frame.
+    
+    We've given you code that keeps track of the segment that you are in.
+    Try to read through the code, to understand what it does.
+    
+    Then scroll down to the next TODO.
+    """
+    if current_segment < len(segment_length):
+        if segment_type[current_segment] == 'straight':
+            distance_covered_in_segment += car_speed * dt
+            if distance_covered_in_segment > segment_length[current_segment]:
+                distance_covered_in_segment = 0.0
                 current_segment += 1
-        else:
+        elif 'turn' in segment_type[current_segment]:
             turned_angle += car_turn_speed * dt
-            if turned_angle > road[current_segment]:
+            if turned_angle > segment_length[current_segment]:
                 turned_angle = 0.0
                 current_segment += 1
 
-        # move the car
-        if current_segment == 0:
-            car_transform = car_transform * Mat4.from_translation(Vec3(car_speed * dt, 0.0, 0.0))
-
-        # turn point 0
-        if current_segment == 1:
-            car_transform = car_transform * Mat4.from_rotation_y(-car_turn_speed * dt, True)
-
-        if current_segment == 2:
-            car_transform = Mat4.from_translation(Vec3(0.0, 0.0, -car_speed * dt)) * car_transform
-
-        # turn point 1
-        if current_segment == 3:
-            car_transform = car_transform * Mat4.from_rotation_y(-car_turn_speed * dt, True)
-
-        if current_segment == 4:
-            car_transform = Mat4.from_translation(Vec3(car_speed * dt, 0.0, 0.0)) * car_transform
-
-        # turn point 2
-        if current_segment == 5:
-            car_transform = car_transform * Mat4.from_rotation_y(car_turn_speed * dt, True)
-
-        if current_segment == 6:
-            car_transform = Mat4.from_translation(Vec3(0.0, 0.0, -car_speed * dt)) * car_transform
-
-        # turn point 3
-        if current_segment == 7:
-            car_transform = car_transform * Mat4.from_rotation_y(-car_turn_speed * dt, True)
-
-        if current_segment == 8:
-            car_transform = Mat4.from_translation(Vec3(car_speed * dt, 0.0, 0.0)) * car_transform
+        current_type = segment_type[current_segment]
+        current_length = segment_length[current_segment]
+        
+        """
+        TODO: Write the code that updates car_transform.
+        HINT: Use your drawing from before to figure out what transformations you need.
+        """
+        car_transform = Mat4.identity() * car_transform
 
     car.set_transform(car_transform)
 
