9eb12e5fa0f2998e94d075bba8be6a7e4691f751
Julia van der Kris <juliavdkris@proton.me>
2025-01-20 16:02:22 +0100

    Spaces->tabs & trim trailing whitespaces

--- 04_building_generation/ex03/main.py
+++ 04_building_generation/ex03/main.py
@@ -44,24 +44,24 @@ in this file. This is how we can reuse code in Python. You can also import
 other Python files in the same way.
 
 Task 1: Check out the Skyscraper class and the code that creates the skyscraper in buildings.py.
-        Then run this file (main.py) and see what it looks like.
-        
+		Then run this file (main.py) and see what it looks like.
+
 Task 2: Check out the components.py file. It contains the methods and classes
-        to generate the components of a building. Recall that in the last two
-        exercises, we have created a basic wall mesh and a wall mesh with a
-        window. Here we just put them in a file so that we can reuse them.
-        
-        The classes in the components.py file are sub-classes of the Mesh class.
-        That means they can be used in places where the program expects a Mesh class,
-        for example, when you add a mesh to the scene:
-        >>> wall = BasicWindowWall(max_width, max_width) # BasicWindowWall is a sub-class of Mesh
-        >>> app.add_mesh(wall)                           # add_mesh expects a Mesh, so wall is a valid argument
-        
-        You can check out how these components are used in the buildings.py file
-        in the Skyscraper class. You can also use them to create other buildings.
+		to generate the components of a building. Recall that in the last two
+		exercises, we have created a basic wall mesh and a wall mesh with a
+		window. Here we just put them in a file so that we can reuse them.
+
+		The classes in the components.py file are sub-classes of the Mesh class.
+		That means they can be used in places where the program expects a Mesh class,
+		for example, when you add a mesh to the scene:
+		>>> wall = BasicWindowWall(max_width, max_width) # BasicWindowWall is a sub-class of Mesh
+		>>> app.add_mesh(wall)                           # add_mesh expects a Mesh, so wall is a valid argument
+
+		You can check out how these components are used in the buildings.py file
+		in the Skyscraper class. You can also use them to create other buildings.
 
 Final assignment: Use what you have learned to create your own signature building.
-        Be creative and have fun!
+		Be creative and have fun!
 """
 
 win = bk.Window()
@@ -71,7 +71,7 @@ win.set_resizable(True)
 
 app = bk.App()
 camera = app.create_camera(
-    pos=Vec3(18, 18, 26), look_at=Vec3(0, 0, 0), fov_v=60.0, background=bk.Color.ICE_BLUE
+	pos=Vec3(18, 18, 26), look_at=Vec3(0, 0, 0), fov_v=60.0, background=bk.Color.ICE_BLUE
 )
 camera.set_as_main_camera()
 
@@ -105,25 +105,25 @@ app.enable_wireframe(enable_wireframe)
 
 @app.event
 def on_update(input, dt, t):
-    global enable_backface_culling
-    global enable_wireframe
-    global is_key_1_pressed
-    global is_key_2_pressed
+	global enable_backface_culling
+	global enable_wireframe
+	global is_key_1_pressed
+	global is_key_2_pressed
 
-    if input.is_key_pressed(bk.KeyCode.Key1):
-        if not is_key_1_pressed:
-            is_key_1_pressed = True
-            enable_backface_culling = not enable_backface_culling
-            app.enable_backface_culling(enable_backface_culling)
-    if input.is_key_released(bk.KeyCode.Key1):
-        is_key_1_pressed = False
-    if input.is_key_pressed(bk.KeyCode.Key2):
-        if not is_key_2_pressed:
-            is_key_2_pressed = True
-            enable_wireframe = not enable_wireframe
-            app.enable_wireframe(enable_wireframe)
-    if input.is_key_released(bk.KeyCode.Key2):
-        is_key_2_pressed = False
+	if input.is_key_pressed(bk.KeyCode.Key1):
+		if not is_key_1_pressed:
+			is_key_1_pressed = True
+			enable_backface_culling = not enable_backface_culling
+			app.enable_backface_culling(enable_backface_culling)
+	if input.is_key_released(bk.KeyCode.Key1):
+		is_key_1_pressed = False
+	if input.is_key_pressed(bk.KeyCode.Key2):
+		if not is_key_2_pressed:
+			is_key_2_pressed = True
+			enable_wireframe = not enable_wireframe
+			app.enable_wireframe(enable_wireframe)
+	if input.is_key_released(bk.KeyCode.Key2):
+		is_key_2_pressed = False
 
 
 app.run(win)
